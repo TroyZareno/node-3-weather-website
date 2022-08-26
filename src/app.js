644770757d5c709bce15d93de12d44bba0b00379
Zareno <francis.troy.zareno@accenture.com>
2022-08-26 22:39:56 +0800

    setup app for heroku

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ console.log(__dirname);
 console.log(path.join(__dirname, "../public"));
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 const publicdirectoryPath = path.join(__dirname, "../public");
 const viewsPath = path.join(__dirname, "../templates/views");
@@ -104,8 +105,8 @@ app.get("*", (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log("Server is up on port 3000");
+app.listen(port, () => {
+  console.log("Server is up on port" + port);
 });
 
 /*
